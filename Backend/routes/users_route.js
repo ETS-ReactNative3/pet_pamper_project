2fb8ebb43cd72d5111300cd017b431d526bc2acc
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-12 16:54:56 +0200

    created signin route

--- Backend/routes/users_route.js
+++ Backend/routes/users_route.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const signupController = require('../controllers/signupController')
 const statusController = require('../controllers/statusController')
+const signinController = require('../controllers/signinController')
 
 app.post('/signup', signupController.addUser)
+app.post('/signin', signinController.getUser)
 app.post('/status', statusController.statusUpdate)
 
 module.exports = app
