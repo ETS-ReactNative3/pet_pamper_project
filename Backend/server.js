cd8c63369c6999336053b7479618552973d58176
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-12 14:22:27 +0200

    installed mongoose module and connected to the mongodb database

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,22 @@ const express = require('express');
 const app = express();
 const usersRoute = require('./routes/users_route');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
+// Database connection
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
 
+db.on('error', (error)=> {console.log(error)})
+db.once('open', ()=> {console.log('connected to database')})
+
+// Body parser with content-type urlencoded
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// Router
 app.use('/user', usersRoute);
 
-
+// Listen to the port
 app.listen(parseInt(process.env.PORT))
