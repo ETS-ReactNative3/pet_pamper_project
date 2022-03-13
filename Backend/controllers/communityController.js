5771624a41fef7be58e06f0a12a464636f251ab8
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-13 18:05:16 +0200

    created a create community controller

--- Backend/controllers/communityController.js
+++ Backend/controllers/communityController.js
@@ -1,10 +1,10 @@
-const User = require('../models/Community')
+const Community = require('../models/Community')
 
 
 exports.addCommunity = (req, res) => { 
     //Check if any of the fields is missing
     try {
-        if (!req.body.name || !req.body.token || !req.body.id || !req.body.phone_number || !req.body.gender || !req.body.account_type) {
+        if (!req.body.name || !req.body.token || !req.body.id || !req.body.account_type) {
             return res.status(400).json({message: "Please don't leave any field empty"})
         }
     } catch (err) {
@@ -12,25 +12,22 @@ exports.addCommunity = (req, res) => {
     }
 
     // Search if the user exist in the database using email parameter
-    User.findOne({email: req.body.email} && {phone_number:  req.body.phone_number}).then((user)=>{
+    Community.findOne({name: req.body.name}).then((community)=>{
 
         //Send that email already exist or add the user to the database
         try{
-            if (user) {
-                return res.status(400).json({message: "user email or phone number already exists"})
+            if (community) {
+                return res.status(400).json({message: "name already exists"})
             }else {
-                const newUser = new User({
-                    first_name: req.body.first_name,
-                    last_name: req.body.last_name,
-                    email: req.body.email,
-                    password: req.body.password,
-                    phone_number: req.body.phone_number,
-                    gender: req.body.gender,
-                    account_type: req.body.account_type
-                })
+                if(req.body.account_type == 'pet walker') {
+                    const newCommunity = new Community({
+                        name: req.body.name,
+                        $push: {members: req.body.id}
+                    })
 
-                newUser.save()
-                return res.status(200).json({message: "Successfully added user"})
+                    newCommunity.save()
+                    return res.status(200).json({message: "Successfully added community"})
+                }else {return res.status(400).json({message: "You are not a pet walker"})}
             }
         }catch(err) {res.send(err)}
     })
