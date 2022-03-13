db283644f807bb196db15950a57f03a98a89402a
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-13 18:47:33 +0200

    added user validation to create community controller

--- Backend/controllers/communityController.js
+++ Backend/controllers/communityController.js
@@ -1,35 +1,42 @@
 const Community = require('../models/Community')
-
+const User = require('../models/User')
 
 exports.addCommunity = (req, res) => { 
     //Check if any of the fields is missing
     try {
-        if (!req.body.name || !req.body.token || !req.body.id || !req.body.account_type) {
+        if (!req.body.name || !req.body.token) {
             return res.status(400).json({message: "Please don't leave any field empty"})
         }
     } catch (err) {
         res.send({message: err})
     }
 
-    // Search if the user exist in the database using email parameter
-    Community.findOne({name: req.body.name}).then((community)=>{
+    User.findOne({token: req.body.token},{_id:1, account_type:1}).then((user) => {
+        if (!user) {
+            return res.status(400).json({message: "You are not a user"})
+        }else {
+            Community.findOne({name: req.body.name}).then((community)=>{
 
-        //Send that email already exist or add the user to the database
-        try{
-            if (community) {
-                return res.status(400).json({message: "name already exists"})
-            }else {
-                if(req.body.account_type == 'pet walker') {
-                    const newCommunity = new Community({
-                        name: req.body.name,
-                        $push: {members: req.body.id}
-                    })
+                try{
+                    if (community) {
+                        return res.status(400).json({message: "name already exists"})
+                    }else {
+                        if(user.account_type == 'pet walker') {
+                            const newCommunity = new Community({
+                                name: req.body.name                       
+                            })
 
-                    newCommunity.save()
-                    return res.status(200).json({message: "Successfully added community"})
-                }else {return res.status(400).json({message: "You are not a pet walker"})}
-            }
-        }catch(err) {res.send(err)}
+                            newCommunity.members.push(user._id)
+                            newCommunity.save()
+
+                            return res.status(200).json({message: "Successfully added community"})
+                            
+                        }else {return res.status(400).json({message: "You are not a pet walker"})}
+                    }
+                }catch(err) {res.send(err)}
+            })
+        }
     })
 }
+
 
