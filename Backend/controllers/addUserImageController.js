6eb4ed4c990444774e47ad1c49e3b8603235a6eb
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-14 18:47:25 +0200

    created gitignore file

--- Backend/controllers/addUserImageController.js
+++ Backend/controllers/addUserImageController.js
@@ -1,18 +1,18 @@
-const User = require('../models/User')
+// const User = require('../models/User')
 
-exports.imageUpdate = function(req, res) {
+// exports.imageUpdate = function(req, res) {
 
 
 
-    User.findOne({'token': req.body.token}).then((user)=>{
-        if (user) {           
-            const updateUserImage = user.set({
-                image: req.body.image,
-            })
-            updateUserImage.save()
-            return res.status(200).json({message: "Successfully updated user status"})
-        }else{
-            return res.status(400).json({message: "You don't have access"})
-        }
-    }).catch((err) => console.log(err))
-}
+//     User.findOne({'token': req.body.token}).then((user)=>{
+//         if (user) {           
+//             const updateUserImage = user.set({
+//                 image: req.body.image,
+//             })
+//             updateUserImage.save()
+//             return res.status(200).json({message: "Successfully updated user status"})
+//         }else{
+//             return res.status(400).json({message: "You don't have access"})
+//         }
+//     }).catch((err) => console.log(err))
+// }
