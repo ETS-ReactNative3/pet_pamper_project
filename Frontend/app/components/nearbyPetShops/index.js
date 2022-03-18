b3e1225116acf9543b5dca6c10cb0b77cb00822f
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-18 19:06:51 +0200

    Frontend: Created the nearby pet shop component

---
+++ Frontend/app/components/nearbyPetShops/index.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import {ScrollView, View, Text, Image, StyleSheet} from 'react-native';
+import { TouchableOpacity } from 'react-native';
+
+const items = [
+    {
+        image: require('../../assets/Pet_Pamper_signIn.png'),
+        text: "Pet Shop 1",
+        location: "Hamra"
+    },
+
+    {
+        image: require('../../assets/Pet_Pamper_signIn.png'),
+        text: "Pet Shop 2",
+        location: "Bliss"
+    },
+
+    {
+        image: require('../../assets/Pet_Pamper_signIn.png'),
+        text: "Pet Shop 3",
+        location: "Verdun"
+    },
+
+    {
+        image: require('../../assets/Pet_Pamper_signIn.png'),
+        text: "Pet Shop 4",
+        location: "Kraytem"
+    },
+
+    {
+        image: require('../../assets/Pet_Pamper_signIn.png'),
+        text: "Pet Shop 5",
+        location: "Rawshe"    },
+];
+
+function NearbyPetShops(props) {
+    return (
+            
+            <ScrollView vertical>
+                {items.map((item,index) => (
+                    <View key= {index}>
+                        <View>
+                            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                                <View>
+                                    <Image style= {styles.image} source= {item.image}/>
+                                </View>
+
+                                <View style= {styles.text}>
+                                    <Text style= {styles.text_title}>{item.text}</Text>
+                                    <Text style= {styles.text_location}>{item.location}</Text>
+                                </View>
+
+                                <View>    
+                                    <TouchableOpacity style= {styles.button}>
+                                        <Text style={styles.button_text}>LOCATE</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                ))}
+            
+            </ScrollView>
+            
+       
+    );
+}
+
+const styles = StyleSheet.create({
+    image: {
+        flexBasis: '10%',
+        width: 50,
+        height: 50,
+        borderRadius: 100,
+        borderColor: 'white',
+        borderWidth: 2,
+        marginTop: 10,
+        marginLeft: 10,
+    },
+
+    text: {
+        flexBasis: '55%',
+        marginLeft: 17,
+        marginTop: 5,
+    },
+
+    text_title: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        color: '#004b67'
+    },
+
+    text_location: {
+        fontSize: 12,
+        color: '#545454'
+    },
+
+    button: {
+        backgroundColor: '#004b67',
+        marginRight: 10,
+        flexBasis: '30%',
+        paddingHorizontal: 12,
+        paddingVertical: 5,
+        borderRadius: 5,
+        
+    },
+
+    button_text: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 14
+    }
+})
+
+export default NearbyPetShops;
