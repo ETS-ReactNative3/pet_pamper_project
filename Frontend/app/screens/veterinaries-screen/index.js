8c52cab1e3c57b624042bc182e1acc4dcc0e4fac
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-19 12:51:45 +0200

    Frontend: Created links between screens

--- Frontend/app/screens/veterinaries-screen/index.js
+++ Frontend/app/screens/veterinaries-screen/index.js
@@ -52,7 +52,7 @@ function VeterinariesScreen({navigation}) {
                         <Icon style={styles.header_icon_arrow} size={20} name="chevron-down"/>
                     </View>
 
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('Notifications')}>
                         <Avatar.Icon style={styles.header_icon} size={40} icon="bell" />
                     </TouchableOpacity>
                 </View>
