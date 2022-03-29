ad8fe908e8eaec2d4f4ce256b5eb13fc017ae531
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-29 17:05:44 +0300

    Frontend: Implemented map view that shows nearby veterinaries

--- Frontend/app/screens/veterinaries-screen/index.js
+++ Frontend/app/screens/veterinaries-screen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native'
+import {ScrollView, View, Text, StyleSheet, TouchableOpacity, Image, Dimensions} from 'react-native'
 import Header from '../../components/header'
 import NearbyVeterinaries from '../../components/nearbyVeterinaries'
 import NavigationBar from '../../components/navigationBar'
@@ -9,10 +9,13 @@ import {useSelector, useDispatch} from 'react-redux'
 import {setUserNearbyVeterinaries} from '../../redux/actions/user-info'
 import * as Linking from 'expo-linking'
 import {getPreciseDistance} from 'geolib'
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
+import MapView, { Callout, Marker } from "react-native-maps";
 
 function VeterinariesScreen({navigation}) {
     const {userToken, userImage, userNearbyVeterinaries, userLatitude, userLongitude} = useSelector(state => state.userReducer)
     const dispatch= useDispatch()
+    const [selectedIndex, setSelectedIndex] = React.useState(0)
     const url = 'http://192.168.1.107:3000/user/veterinaries'
 
     React.useEffect(async ()=> {
@@ -66,61 +69,77 @@ function VeterinariesScreen({navigation}) {
                     </TouchableOpacity>
                 </View>
 
-                <Text style={styles.header_sub_title}>Nearby veterinaries</Text>
+                <View>
+                    <SegmentedControl
+                        values={['List', 'Map']}
+                        selectedIndex={selectedIndex}
+                        onChange={(event) => {
+                        setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
+                        }}
+                        style= {styles.list_controller}
+                    />
+                </View> 
             </View>
 
+            {selectedIndex == 0 ?
             <View>
-                <ScrollView>
-                    {nv_items.map((nv_item, nv_index) => (
-                        <View key= {nv_index}>
-                            <View>
-                                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                                    <View>
-                                        <Image style= {styles.nv_image} source= {{uri: `data:image/gif;base64,${nv_item.image}`}}/>
-                                    </View>
+                <Text style={styles.header_sub_title}>Nearby veterinaries</Text>
+                
+                <View>
+                    <ScrollView>
+                        {nv_items.map((nv_item, nv_index) => (
+                            <View key= {nv_index}>
+                                <View>
+                                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                                        <View>
+                                            <Image style= {styles.nv_image} source= {{uri: `data:image/gif;base64,${nv_item.image}`}}/>
+                                        </View>
 
-                                    <View style= {styles.nv_text}>
-                                        <Text style= {styles.nv_text_title}>{nv_item.first_name}</Text>
-                                        {/* <Text style= {styles.nv_text_location}>{nv_item.location}</Text> */}
-                                    </View>
+                                        <View style= {styles.nv_text}>
+                                            <Text style= {styles.nv_text_title}>{nv_item.first_name}</Text>
+                                            {/* <Text style= {styles.nv_text_location}>{nv_item.location}</Text> */}
+                                        </View>
 
-                                    <View>    
-                                        <TouchableOpacity style= {styles.nv_button} onPress= {()=> locateVeterinary(nv_item)}>
-                                            <Text style={styles.nv_button_text}>LOCATE</Text>
-                                        </TouchableOpacity>
+                                        <View>    
+                                            <TouchableOpacity style= {styles.nv_button} onPress= {()=> locateVeterinary(nv_item)}>
+                                                <Text style={styles.nv_button_text}>LOCATE</Text>
+                                            </TouchableOpacity>
+                                        </View>
                                     </View>
                                 </View>
                             </View>
-                        </View>
-                    ))}
-                
-                </ScrollView>
-            </View>
-
-            <View style={styles.nav_bar}>
-                <View style={styles.nav_icon_area}>
-
-                    <TouchableOpacity style={{flex: 1}} onPress={() => navigation.navigate('Explore')}>
-                        <Icon style={styles.nav_icon_discover} color= "#80f7e3" size={30} name="group"/>
-                        <Text style= {{color:'#80f7e3'}}>Discover</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={{flex: 1.1}} onPress={() => navigation.navigate('Veterinaries')}>
-                        <Icon  style={styles.nav_icon_vet} color= "#80f7e3" size={30} name="medkit"/>
-                        <Text style= {{color:'#80f7e3'}}>Veterinary</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={{flex: 1.02}} onPress={() => navigation.navigate('Pet Shops')}>
-                        <Icon style={styles.nav_icon_shop} color= "#80f7e3" size={30} name="paw"/>
-                        <Text style= {{color:'#80f7e3'}}>Pet Shop</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={{flex: 1.}} onPress={() => navigation.navigate('Profile')}>
-                        <Icon style={styles.nav_icon_profile} color= "#80f7e3" size={30} name="user-circle-o"/>
-                        <Text style= {{color:'#80f7e3'}}>Profile</Text>
-                    </TouchableOpacity>
+                        ))}
+                    
+                    </ScrollView>
                 </View>
-            </View>
+            </View> :
+
+            <View>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                    latitude: parseFloat(userLatitude),
+                    longitude: parseFloat(userLongitude),
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                    }}
+                    provider="google"
+                    showsUserLocation= {true}
+                    loadingEnabled= {true}>
+
+                    {nv_items.map((nv_item, nv_index) => (                 
+                        <View key= {nv_index}>
+                            <Marker coordinate={{latitude: parseFloat(nv_item.latitude), longitude: parseFloat(nv_item.longitude)}} 
+                            pinColor="red" 
+                            onPress= {()=>locateVeterinary(nv_item)}>                                                            
+                                <Callout style={{width: 100, alignItems:'center'}}>   
+                                    <Text>{nv_item.first_name}</Text>   
+                                </Callout>
+                            </Marker>
+                        </View>
+                    ))}              
+                </MapView>
+            </View>}
 
         </View>
     );
@@ -135,8 +154,7 @@ const styles = StyleSheet.create({
     header_area: {
         borderColor: 'white',
         width: '100%',
-        height: 100,
-        marginBottom: 10
+        height: 110,
     },
     
     header: {
@@ -193,7 +211,8 @@ const styles = StyleSheet.create({
         fontSize: 19,
         marginTop: 10,
         marginLeft: 15,
-        color: 'black'
+        color: 'black',
+        marginBottom: 10
     },
 
     nv_image: {
@@ -269,6 +288,20 @@ const styles = StyleSheet.create({
 
     nav_icon_profile: {
         marginLeft: 5
+    },
+
+    container: {
+        flex: 1,
+      },
+    
+    map: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+    },
+
+    list_controller: {
+        height: 35,
+        marginTop: 10
     },
 
 })
