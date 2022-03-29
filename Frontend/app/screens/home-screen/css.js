cdfc172b65eef8af86d6bc700c7df08e1e3e2927
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-29 15:15:04 +0300

    Frontend: Home screen CSS cleanup

---
+++ Frontend/app/screens/home-screen/css.js
@@ -0,0 +1,122 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    backgroudArea: {
+        flex:1,
+        backgroundColor: "#004b67",
+    },
+    
+    image: {
+        flex:1.3,
+        height: '100%',
+        width: '100%',
+        resizeMode:"stretch"
+    },
+    
+    googleButton: {
+        flex:1,
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        alignItems: 'center'
+    },
+
+    line: {
+        backgroundColor: '#004b67',
+        width: '90%',
+        marginTop: 20,
+        height: 1
+    },
+
+    orText: {
+        position: 'absolute',
+        top: 105,
+        fontSize: 19,
+        zIndex: 1,
+        backgroundColor: 'white',
+        padding: 5
+    },
+
+    signup: {
+        marginTop: 20
+    },
+
+    button_area_signin: {
+        borderWidth: 1,
+        borderColor: '#518ef8',
+        borderRadius: 7,
+        width: '90%',
+        height: 55,
+        textTransform: 'capitalize',
+        marginTop: 40,
+        marginBottom: 10
+    },
+    
+    button_signin: {
+        flex: 1,
+        flexDirection: 'row',
+        borderRadius: 5,
+        justifyContent: 'center',
+        backgroundColor: '#518ef8',
+        width: '100%',
+        alignItems: 'center',   
+    },
+
+    text_signin: {
+        paddingLeft: 15,
+        flexBasis: '80%',
+        fontSize: 17,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+
+    googleIconArea: {
+        marginLeft: 2,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexBasis: '15%',
+        width: '90%',
+        height: '90%',
+        backgroundColor: 'white',
+        resizeMode: 'contain',
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 5
+    },
+
+    googleIcon: {
+        justifyContent: 'center',
+        width: '90%',
+        height: '90%',
+        resizeMode: 'contain',
+    },
+
+    buttonArea: {
+        borderWidth: 1,
+        borderColor: '#518ef8',
+        borderRadius: 7,
+        width: '90%',
+        height: 55,
+        textTransform: 'capitalize',
+        marginTop: 30
+    },
+    
+    button: {
+        flex: 1,
+        flexDirection: 'row',
+        borderRadius: 5,
+        justifyContent: 'center',
+        backgroundColor: '#004b67',
+        width: '100%',
+        alignItems: 'center',   
+    },
+
+    text: {
+        paddingLeft: 15,
+        fontSize: 17,
+        color: '#80f7e3',
+        fontWeight: 'bold',
+    },
+})
