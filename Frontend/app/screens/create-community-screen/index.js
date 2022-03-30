133efd639f3cf0a9e9ea5f4a662ab6233ac28068
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-30 13:47:43 +0300

    Frontend: Created a header for the create community screen

--- Frontend/app/screens/create-community-screen/index.js
+++ Frontend/app/screens/create-community-screen/index.js
@@ -37,24 +37,6 @@ function CreateCommunityScreen({navigation}) {
 
     return (
         <View style={styles.background}>
-            {/* <View style={styles.header_area}>
-                <View style={styles.header}>              
-                    <TouchableOpacity onPress={()=> navigation.navigate('Profile Screen')}>
-                        <Avatar.Icon style={styles.header_icon} color='black' size={60} icon="chevron-left" />
-                    </TouchableOpacity>
-
-                    <View style={styles.header_text_area}>
-                        <Text style={styles.header_text}>Create Community</Text>
-                        <Icon style={styles.header_icon_arrow} size={20} name="chevron-down"/>
-                    </View>
-
-                    <TouchableOpacity onPress={()=>navigation.navigate('Notifications')}>
-                        <Avatar.Icon style={styles.header_icon} size={40} icon="bell" />
-                    </TouchableOpacity>
-                </View>
-
-                <Text style={styles.header_sub_title}></Text>
-            </View> */}
             <CreateCommunityHeader navigation={navigation}/>
             <View style= {styles.backgroudArea}>
 
