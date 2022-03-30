98a4a03515872d0a8ed98f17eb15c9ef140121ee
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-03-30 14:14:31 +0300

    Frontend: Created input form component for create community screen

--- Frontend/app/screens/create-community-screen/index.js
+++ Frontend/app/screens/create-community-screen/index.js
@@ -1,77 +1,13 @@
-import React, {useEffect} from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
-import { Avatar, TextInput } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/FontAwesome';
-// import * as Location from 'expo-location';
-// import * as ImagePicker from 'expo-image-picker';
-import {useSelector} from 'react-redux'
+import {View} from 'react-native'
 import {styles} from './css'
-import {userCreateCommunity} from '../../services'
-import {userCreateCommunityImage, userCreateCommunityLocation} from '../../methods'
 import CreateCommunityHeader from './header';
+import InputForm from './input-form';
 
-function CreateCommunityScreen({navigation}) {
-    const {userToken} = useSelector(state => state.userReducer)
-    const [image, setImage] = React.useState(null);
-    const [image_base64, setImageBase64] = React.useState(null);
-    const [name, onChangeName] = React.useState("");
-    const [status, setStatus] = React.useState("");
-    const [latitude, setLatitude] = React.useState("");
-    const [longitude, setLongitude] = React.useState("");
-
-
-    const getCommunityLocation = async () => {
-        let location = await userCreateCommunityLocation()       
-        setLatitude(location.coords.latitude);
-        setLongitude(location.coords.longitude);
-      };
-
-    const pickImage = async () => {
-        let result = await userCreateCommunityImage()       
-        setImageBase64(result.base64);
-        setImage(result.uri)
-      };
-
-
-
-
+export default function CreateCommunityScreen({navigation}) {
     return (
         <View style={styles.background}>
             <CreateCommunityHeader navigation={navigation}/>
-            <View style= {styles.backgroudArea}>
-
-                <View style={styles.edit_form}>
-
-                    <Text>{status}</Text>
-
-                    <TextInput
-                        style= {styles.name_input}
-                        mode="outlined"
-                        label="Community Name"
-                        placeholder="Enter Community Name"
-                        onChangeText={newText => onChangeName(newText)}
-                    /> 
-
-                    <View style={styles.picked_image_area}>
-                        <TouchableOpacity onPress={()=> {pickImage(), getCommunityLocation()}} style={styles.picked_image_button}>
-                            <Text style={styles.picked_image_text}>Choose an image</Text>
-                        </TouchableOpacity>
-                        {/* {image &&<Text>{image}</Text>} */}
-                        {image && <Image source={{ uri: image}} style={styles.picked_image} />}
-                    </View>
-
-                    <View  style={styles.button_area_create}>
-                        <TouchableOpacity style={styles.button_create} onPress={async ()=> setStatus(await userCreateCommunity(name, userToken, image_base64, latitude, longitude))}>
-                            <Text style={styles.text_create}>CREATE</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-
-    </View>
+            <InputForm/>
+        </View>
     )
 }
-
-
-
-export default CreateCommunityScreen;
---
+++ Frontend/app/screens/create-community-screen/input-form.js
@@ -0,0 +1,61 @@
+import {setUserLastName, setUserFirstName, setUserPassword} from '../../redux/actions/user-info'
+import {useSelector, useDispatch} from 'react-redux'
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native'
+import { TextInput } from 'react-native-paper';
+import {styles} from './css'
+import {userCreateCommunity} from '../../services'
+import {userCreateCommunityImage, userCreateCommunityLocation} from '../../methods'
+
+export default function InputForm() {
+    const dispatch = useDispatch()  
+    const {userToken} = useSelector(state => state.userReducer)
+    const [image, setImage] = React.useState(null);
+    const [image_base64, setImageBase64] = React.useState(null);
+    const [name, onChangeName] = React.useState("");
+    const [status, setStatus] = React.useState("");
+    const [latitude, setLatitude] = React.useState("");
+    const [longitude, setLongitude] = React.useState("");
+    
+    const getCommunityLocation = async () => {
+        let location = await userCreateCommunityLocation()       
+        setLatitude(location.coords.latitude);
+        setLongitude(location.coords.longitude);
+      };
+
+    const pickImage = async () => {
+        let result = await userCreateCommunityImage()       
+        setImageBase64(result.base64);
+        setImage(result.uri)
+      };
+    
+    return (
+        <View style= {styles.backgroudArea}>
+            <View style={styles.edit_form}>
+                <Text>{status}</Text>
+
+                <TextInput
+                    style= {styles.name_input}
+                    mode="outlined"
+                    label="Community Name"
+                    placeholder="Enter Community Name"
+                    onChangeText={newText => onChangeName(newText)}
+                /> 
+
+                <View style={styles.picked_image_area}>
+                    <TouchableOpacity onPress={()=> {pickImage(), getCommunityLocation()}} style={styles.picked_image_button}>
+                        <Text style={styles.picked_image_text}>Choose an image</Text>
+                    </TouchableOpacity>
+                    
+                    {image && <Image source={{ uri: image}} style={styles.picked_image} />}
+                </View>
+
+                <View  style={styles.button_area_create}>
+                    <TouchableOpacity style={styles.button_create} onPress={async ()=> setStatus(await userCreateCommunity(name, userToken, image_base64, latitude, longitude))}>
+                        <Text style={styles.text_create}>CREATE</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+}
